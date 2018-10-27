773480c4021397922d7457053fc7ca9f32504965
haiboLiao <676310501@qq.com>
2018-10-27 21:53:47 +0800

    java learn

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import classLearn.Person;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Person person = new Person();
+        person.setName("小明");
+        person.setAge(28);
+        System.out.println("person.name = " + person.getName());
+        System.out.println("person.age = " + person.getAge());
     }
 }
---
+++ src/classLearn/Person.java
@@ -0,0 +1,29 @@
+package classLearn;
+
+/**
+ * @author Liaohaibo
+ * @date 2018/10/27 下午9:48
+ */
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        name = "xiaoming";
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
